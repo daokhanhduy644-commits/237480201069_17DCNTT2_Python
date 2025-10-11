3b6ab8fc2b8414d9847e9a5bcc2b3cfef61a7b5d
tran nghia <trannghia2001bl@gmail.com>
2025-10-11 10:16:52 +0700

    cap nhat 11-10-2025

---
+++ BAITAPMAU/bai5_tr45.py
@@ -0,0 +1,3 @@
+n=int(input("Nhập vào số nguyên dương n:"))
+if n>10:
+    print(n,"là số lớn hơn 10")4
---
+++ BAITAPMAU/bai6_tr45.py
@@ -0,0 +1,5 @@
+n=int(input("Nhập vào số nguyên dương n:"))
+if(n%2==0):
+    print(n,"là số chẵn")
+else:
+    print(n,"là số lẻ")
